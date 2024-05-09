c20b9dfd8086d3b6218d02109d5d86db24e5b287
SeaSm0kee <iantaulam@alu.edu.gva.es>
2024-05-09 18:11:43 +0200

    Pistol

---
+++ Proyecto final/Assets/Scripts/FireBulletOnActivate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class FireBulletOnActivate : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float fireSpeed;
+    [SerializeField] private Transform spawnPoint;
+    void Start()
+    {
+        XRBaseInteractable grabbable = GetComponent<XRBaseInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 10);
+    }
+}
